c0752087ad488c8a7bae2c3dc87f8338d495bbac
Han Fei <hanfei19910905@gmail.com>
2019-11-10 14:34:38 +0800

    tmp support if

--- main.jl
+++ main.jl
@@ -5,9 +5,30 @@ struct PrimType
     name::String
 end
 
+struct BottomType
+end
+
 function inferExpr(expr)
     if typeof(expr) === Int64
         return PrimType("Int64")
+    end
+end
+
+function inferSeq(ast, env)
+    if (ast.head == Symbol("="))
+        tp = inferExpr(ast.args[2])
+        env = bind(ast.args[1], tp, env)
+        return BottomType(), env
+    elseif (ast.head == :block)
+        for arg in ast.args
+            tp , env = inferSeq(ast, env)
+            if (typeof(tp) != BottomType)
+                return tp, env
+            end
+        end
+        return BottomType(), env
+    elseif (ast.head == :LineNumberNode)
+        return BottomType(), env
     end
 end
 
@@ -15,16 +36,17 @@ function bind(l, r, env)
     return Pair(Pair(l,r), env)
 end
 
-env1 = nothing
-
-for i = 1:length(str_arr)
-    ast = Meta.parse(str_arr[i]);
-    dump(ast)
-    if (ast.head == Symbol("="))
-        tp = inferExpr(ast.args[2])
-        env = bind(ast.args[1], tp, env1)
-    elseif (ast.head == Symbol("if"))
-        println("if")
+function main(env, str_arr)
+    for i = 1:length(str_arr)
+        ast = Meta.parse(str_arr[i]);
+        dump(ast)
+        if (ast.head == Symbol("="))
+            tp, env = inferSeq(ast, env)
+        elseif (ast.head == Symbol("if"))
+            t1, env1 = inferSeq(ast.args[2])
+        end
     end
 end
 
+env = nothing
+main(env, str_arr)
--- test1.jl
+++ test1.jl
@@ -1,2 +1,3 @@
 a1 = 5
-if (a1 < 6) a1 = "str"; else a1 = 10.0; end
+if (a1 < 6) a1 = "str"; return a1; elseif(a1 > 5) a1 = 10.0; else a1 = 3 end
+if (a1 < 6) a1 = "str";  end
